6b2950f5d03636fe06de71732f176c1b1c76edbf
rodolfoaugusto <rodolfoaugusto@hotmail.com.br>
2022-02-14 09:45:24 -0300

    fix: IP Rate Limit disabled

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import cors from 'cors';
 import helmet from 'helmet';
 import routes from './routes';
 import errorHandler from './middlewares/error';
-import ipRateLimit from './ip';
 
 const app = express();
 
@@ -13,6 +12,5 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 app.use(errorHandler);
-app.use(ipRateLimit);
 
 export default app;
